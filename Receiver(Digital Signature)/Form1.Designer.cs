7dadefe5e4454e5b47393b0ee63627edbe38003d
Dainoras Žiukas <65944827+dainoras99@users.noreply.github.com>
2021-06-06 14:00:33 +0300

    Made Server/Client connection

--- Receiver(Digital Signature)/Form1.Designer.cs
+++ Receiver(Digital Signature)/Form1.Designer.cs
@@ -37,6 +37,13 @@ namespace Receiver_Digital_Signature_
             this.digitalSignatureFromTheSender = new System.Windows.Forms.TextBox();
             this.confirmTheSignatureButton = new System.Windows.Forms.Button();
             this.signatureConfirmedLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.stop = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -62,7 +69,7 @@ namespace Receiver_Digital_Signature_
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(111, 62);
+            this.label3.Location = new System.Drawing.Point(111, 152);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(126, 13);
             this.label3.TabIndex = 5;
@@ -70,7 +77,7 @@ namespace Receiver_Digital_Signature_
             // 
             // messageFromTheSender
             // 
-            this.messageFromTheSender.Location = new System.Drawing.Point(12, 78);
+            this.messageFromTheSender.Location = new System.Drawing.Point(12, 168);
             this.messageFromTheSender.Name = "messageFromTheSender";
             this.messageFromTheSender.Size = new System.Drawing.Size(339, 20);
             this.messageFromTheSender.TabIndex = 4;
@@ -78,7 +85,7 @@ namespace Receiver_Digital_Signature_
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(85, 113);
+            this.label4.Location = new System.Drawing.Point(85, 203);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(187, 13);
             this.label4.TabIndex = 7;
@@ -86,14 +93,14 @@ namespace Receiver_Digital_Signature_
             // 
             // digitalSignatureFromTheSender
             // 
-            this.digitalSignatureFromTheSender.Location = new System.Drawing.Point(12, 129);
+            this.digitalSignatureFromTheSender.Location = new System.Drawing.Point(12, 219);
             this.digitalSignatureFromTheSender.Name = "digitalSignatureFromTheSender";
             this.digitalSignatureFromTheSender.Size = new System.Drawing.Size(339, 20);
             this.digitalSignatureFromTheSender.TabIndex = 6;
             // 
             // confirmTheSignatureButton
             // 
-            this.confirmTheSignatureButton.Location = new System.Drawing.Point(114, 174);
+            this.confirmTheSignatureButton.Location = new System.Drawing.Point(114, 264);
             this.confirmTheSignatureButton.Name = "confirmTheSignatureButton";
             this.confirmTheSignatureButton.Size = new System.Drawing.Size(131, 23);
             this.confirmTheSignatureButton.TabIndex = 8;
@@ -103,18 +110,88 @@ namespace Receiver_Digital_Signature_
             // signatureConfirmedLabel
             // 
             this.signatureConfirmedLabel.AutoSize = true;
-            this.signatureConfirmedLabel.Location = new System.Drawing.Point(159, 217);
+            this.signatureConfirmedLabel.Location = new System.Drawing.Point(159, 307);
             this.signatureConfirmedLabel.Name = "signatureConfirmedLabel";
             this.signatureConfirmedLabel.Size = new System.Drawing.Size(35, 13);
             this.signatureConfirmedLabel.TabIndex = 9;
             this.signatureConfirmedLabel.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-34, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(439, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "________________________________________________________________________";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(212, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Port:";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(247, 62);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(77, 20);
+            this.portTextBox.TabIndex = 21;
+            this.portTextBox.Text = "8087";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 65);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "IP:";
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(46, 62);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(158, 20);
+            this.ipTextBox.TabIndex = 19;
+            this.ipTextBox.Text = "127.0.0.1";
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(195, 88);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 18;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(102, 88);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 17;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
-            this.ClientSize = new System.Drawing.Size(363, 239);
+            this.ClientSize = new System.Drawing.Size(363, 339);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.ipTextBox);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.signatureConfirmedLabel);
             this.Controls.Add(this.confirmTheSignatureButton);
             this.Controls.Add(this.label4);
@@ -125,6 +202,7 @@ namespace Receiver_Digital_Signature_
             this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Receiver";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +218,13 @@ namespace Receiver_Digital_Signature_
         private System.Windows.Forms.TextBox digitalSignatureFromTheSender;
         private System.Windows.Forms.Button confirmTheSignatureButton;
         private System.Windows.Forms.Label signatureConfirmedLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button start;
     }
 }
 
--- Receiver(Digital Signature)/Form1.cs
+++ Receiver(Digital Signature)/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleTCP;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,37 @@ namespace Receiver_Digital_Signature_
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+        }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            messageFromTheSender.Invoke((MethodInvoker)delegate ()
+            {
+                messageFromTheSender.Text += e.MessageString;
+            });
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(ipTextBox.Text);
+            server.Start(ip, Convert.ToInt32(portTextBox.Text));
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+                server.Stop();
         }
     }
 }
